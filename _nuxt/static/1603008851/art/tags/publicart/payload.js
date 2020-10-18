6c43d7ba579b2722936edb40d3939eb45cee853e
Lau Tiam kok <lau.tiamkok@yahoo.co.uk>
2020-10-18 10:14:44 +0200

    6096dd4

---
+++ _nuxt/static/1603008851/art/tags/publicart/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/art/tags/publicart", (function(a,b){return {data:[{projects:[{title:a,directory:"art",slug:"twenties",path:"art\u002Fpublished\u002Ftwenties",year:"2006",tags:[b],coverImage:{file:"Twenties_01.jpg",title:a,alt:null}}],tag:b}],fetch:[],mutations:[]}}("Twenties","publicart")));
